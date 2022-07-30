68304351d19409b5fa508796eec0bd8247ed1dbc
Abdallah <abdlaa1912@gmail.com>
2022-07-30 12:54:26 +0300

    designed the style of the website and the index page

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ server.use(express.static("./public"));
 server.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+server.get("/index", (req, res) => {
+    res.redirect("/");
+});
+server.get("/home", (req, res) => {
+    res.redirect("/");
+});
 
 server.listen(PORT, ()=>{
     console.log(`Listening on http://localhost:${PORT}`);
